ac624a28a83a230f882f1ed0f6ba95167f65486a
duanbin218 <389749524@qq.com>
2025-12-19 13:19:11 +0800

    修改config配置文件,把kmNET等参数添加进config

--- test/config.py
+++ test/config.py
@@ -10,3 +10,15 @@ DICT_SYS_PATH = "./字库/系统字库 - 副本.txt"
 DICT_PLAYER_PATH = "./字库/玩家字库.txt"
 
 MAP_IMAGE_PATH = "xinrenditu.bmp"
+
+# kmNet connection settings (init_runtime defaults).
+KMNET_IP = "192.168.2.188"
+KMNET_PORT = "1538"
+KMNET_TOKEN = "86C2E466"
+
+# Map/coordinate settings used by kmNet controls.
+MAP_CENTER_X = 964
+MAP_CENTER_Y = 464
+TILE_WIDTH = 48
+TILE_HEIGHT = 32
+DEFAULT_CIRCLE_RADIUS = 170
